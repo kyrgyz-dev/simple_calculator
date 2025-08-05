8e170d56eb30532e51507ba5b4e3d98ae6db879f
kyrgyz-dev <okurman.kenzhegulov@gmail.com>
2025-08-05 12:27:29 +0600

    divide metodu bergen joopko shart jazyldy

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from calculator import add, subtract, multiply, divide
 
 a = 10
-b = 5
+b = 0
 
 print(f'{a} + {b} = {add(a, b)}')
 print(f'{a} - {b} = {subtract(a, b)}')
 print(f'{a} * {b} = {multiply(a, b)}')
-print(f'{a} / {b} = {divide(a, b)}')
+res = divide(a, b)
+if res is not None:
+    print(f'{a} / {b} = {divide(a, b)}')
