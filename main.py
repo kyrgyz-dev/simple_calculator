43679f90c5f0e669d332259bea48d8451098b790
kyrgyz-dev <okurman.kenzhegulov@gmail.com>
2025-08-05 11:12:29 +0600

    multiply metodun koshtuk

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-from calculator import add, subtract
+from calculator import add, subtract, multiply
 
 a = 10
 b = 5
 
 print(f'{a} + {b} = {add(a, b)}')
 print(f'{a} - {b} = {subtract(a, b)}')
+print(f'{a} * {b} = {multiply(a, b)}')
